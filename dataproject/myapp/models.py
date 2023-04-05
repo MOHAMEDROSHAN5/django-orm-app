6339ac9a6e7a886554a68d95024f56f2e5fed05b
MOHAMEDROSHAN5 <mohamedroshan7405@gmail.com>
2023-04-05 16:15:02 +0000

    first commit

---
+++ dataproject/myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib import admin
+
+
+# Create your models here.
+class Student (models.Model):
+    referenceno=models.CharField(max_length=20,help_text="reference number")
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField()
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('referenceno','name','age','email')
